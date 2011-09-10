5c1517a99c54f3573645456d4e56594e10a5946d
Joaquin Caraballo <jcaraballo.git@gmail.com>
2011-09-10 23:00:30 +0100

    Added Concordion specification and fake wiring test clase (that I missed in the previous push).

---
+++ src/test-acceptance/concordion/ListaPermaneceVaciaTest.java
@@ -0,0 +1,9 @@
+package concordion;
+
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+public class ListaPermaneceVaciaTest extends ConcordionTestCase {
+    public String articleListAfterAdding(String url) {
+        return "undefined";
+    }
+}
